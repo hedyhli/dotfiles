0446ba195451447001addf6aab15834f6095e4e2
Hedy Li <hedy@tilde.cafe>
2021-08-18 16:52:42 +0800

    PLEASE can tmux true colors finally work????? gosh

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,11 @@ end
 if test -f ~/.config/fish/config_local.fish
 	source ~/.config/fish/config_local.fish
 end
+
+set -gx HOMEBREW_PREFIX "/home/hedy/.linuxbrew";
+set -gx HOMEBREW_CELLAR "/home/hedy/.linuxbrew/Cellar";
+set -gx HOMEBREW_REPOSITORY "/home/hedy/.linuxbrew/Homebrew";
+set -gx HOMEBREW_SHELLENV_PREFIX "/home/hedy/.linuxbrew";
+set -q PATH; or set PATH ''; set -gx PATH "/home/hedy/.linuxbrew/bin" "/home/hedy/.linuxbrew/sbin" $PATH;
+set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/home/hedy/.linuxbrew/share/man" $MANPATH;
+set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/home/hedy/.linuxbrew/share/info" $INFOPATH;
