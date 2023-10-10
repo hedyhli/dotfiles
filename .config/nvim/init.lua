cf1ee79575cbfde4ee2eed8a41114e042d7403ca
hedy <hedy@tilde.cafe>
2023-10-10 19:15:19 +0800

    nvim: Add comment about setting leader to space
    Was already setting leader to space in nvim due to muscle memory from
    doom emacs months ago, but I kept the `maplocalleader` setting. When I
    found out about t/T/f/F/; commands I decided to drop them entirely and
    leave a note, as with adjusting it in vim.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = [[ ]]
-vim.g.maplocalleader = [[;]]
--- The cool thing about ';' is that it is at the same
--- position as ':', the char used for commands.
+-- vim.g.maplocalleader = [[;]]
+-- See my vim config comment on me being stupid enough to not
+-- have known f, t, F, T and ';' 😔
 
 -- Set envvar VIMHOME to, eg, ~/.config/nvim (has no trailing slash)
 vim.env.VIMHOME = vim.fn.expand('<sfile>:p:h')
