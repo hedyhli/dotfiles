da6fc68aa458f13af678d9c055f7a975d22a7175
~hedy <hedy.dev@protonmail.com>
2024-09-26 21:25:05 +0800

    Fish: Use more portable characters in VCS prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -34,13 +34,13 @@ function fish_prompt --description 'Write out the prompt'
         set -g __fish_git_prompt_char_stagedstate "●"
     end
     if not set -q __fish_git_prompt_char_dirtystate
-        set -g __fish_git_prompt_char_dirtystate "✚"
+        set -g __fish_git_prompt_char_dirtystate "+"
     end
     if not set -q __fish_git_prompt_char_untrackedfiles
         set -g __fish_git_prompt_char_untrackedfiles "…"
     end
     if not set -q __fish_git_prompt_char_invalidstate
-        set -g __fish_git_prompt_char_invalidstate "✖"
+        set -g __fish_git_prompt_char_invalidstate "x"
     end
     if not set -q __fish_git_prompt_char_cleanstate
         # Switch from the check symbol to = because this fixes the width
